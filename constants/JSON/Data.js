943c1630b6678729c83b37f3e50220093f7eb660
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-09-01 23:01:11 +0500

    data for top-navigation set

---
+++ constants/JSON/Data.js
@@ -0,0 +1,24 @@
+const Data = {
+  TopNavigation: {
+    NavItems: [
+      {
+        Name: "Navbar.Links.FindTransporter", //Find a carrier
+        Route: "/",
+      },
+      {
+        Name: "Navbar.Links.Yearly", //Phone book
+        Route: "/annuaire",
+      },
+      {
+        Name: "Navbar.Links.TrackAnItem", //Track a package
+        Route: "",
+      },
+      {
+        Name: "Navbar.Links.FiamaneCompany", //Fiamane company
+        Route: "",
+      },
+    ],
+  },
+};
+
+export default Data;
